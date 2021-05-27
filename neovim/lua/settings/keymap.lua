d595c8ddcf87eb392b8d4e2ee76f8d419d409746
Brendan Mulholland <github@bmulholland.ca>
2021-05-27 11:25:10 +0200

    Keymapping improvements

--- neovim/lua/settings/keymap.lua
+++ neovim/lua/settings/keymap.lua
@@ -22,50 +22,50 @@ map('v', '<leader>p', '"_dP') -- Paste multiple times with the same contents
 -- in a view the "alternate" is the spec and the "related" is the controller (method)
 -- Easily switch between file and its spec using rails.vim's alternate file functionality
 map('n', '<leader>a', ':A<CR>')
--- Easily switch between controllers and views using rails.vim's related file functionality
-map('n', '<leader>r', ':R<CR>')
-
--- Navigate between splits
--- Use ctrl-[hjkl] to select the active split
-map('n', '<c-k>', ':wincmd k<CR>', { silent = true })
-map('n', '<c-j>', ':wincmd j<CR>', { silent = true })
-map('n', '<c-h>', ':wincmd h<CR>', { silent = true })
-map('n', '<c-l>', ':wincmd l<CR>', { silent = true })
 
 -- Move the splits arround - but sadly couldn't get it working
---map('n', '<c-s-k>', '<C-W>K', { silent = true })
---map('n', '<c-s-j>', '<C-W>J', { silent = true })
---map('n', '<c-s-h>', '<C-W>H', { silent = true })
---map('n', '<c-s-l>', '<C-W>L', { silent = true })
+--map('n', '<c-s-k>', '<C-W>K', {silent = true })
+--map('n', '<c-s-j>', '<C-W>J', {silent = true })
+--map('n', '<c-s-h>', '<C-W>H', {silent = true })
+--map('n', '<c-s-l>', '<C-W>L', {silent = true })
 
 -- Navigate within a file
 -- Go to next lint error
 map('n', '<leader>e', '<Plug>(ale_next_wrap)')
 
 -- Open plugins
-map('n', '<leader>t', ":lua require('plugins.telescope').project_files()<CR>", { silent = true})
-
--- TODO: Learn these too
--- From https://github.com/crivotz/nv-ide/blob/master/lua/settings/keymap.lua#L41
-vim.api.nvim_set_keymap('n', '<leader>r', ":lua require('telescope.builtin').live_grep()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>bb', ":lua require('telescope.builtin').buffers()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>m', ":lua require('telescope.builtin').marks()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>e', ":lua require('telescope.builtin').treesitter()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>l', ":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>p', ":lua require('telescope').extensions.project.project{}<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>c', ":lua require('plugins.telescope').my_git_commits()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gg', ":lua require('plugins.telescope').my_git_status()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>b', ":lua require('plugins.telescope').my_git_bcommits()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>n', ":lua require('plugins.telescope').my_note()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>nn', ":e ~/Note/", {noremap = true, silent = false})
-vim.api.nvim_set_keymap('n', '<leader>gc', ':Octo issue create<CR>', {noremap = true, silent = false})
-vim.api.nvim_set_keymap('n', '<leader>i', ':Octo issue list<CR>', {noremap = true, silent = false})
+map('n', '<leader>t', ":lua require('plugins.telescope').project_files()<CR>", {silent = true})
 
 map('n', '<leader>n', ':NvimTreeToggle<cr>') -- Open a file explorer
 
-map('n', '<leader>s', ':TagbarToggle<CR>') -- Open tagbar
-
-map('n', '<leader>d', ':Dash<CR>', { silent = true }) -- Look up command in Dash
+map('n', '<leader>d', ':Dash<CR>', {silent = true }) -- Look up command in Dash
 
 -- Pretty print JSON
 map('n', '<leader>o', ':%!python -m json.tool<CR>')
+
+-- LSP - Errors, Definitions, etc
+map('n', '<leader>,', ':lua vim.lsp.diagnostic.goto_prev()<CR>', {silent = true})
+map('n', '<leader>;', ':lua vim.lsp.diagnostic.goto_next()<CR>', {silent = true})
+map('n', '<leader>a', ':lua vim.lsp.diagnostic.code_action()<CR>', {silent = true})
+map('n', '<leader>d', ':lua vim.lsp.buf.definition()<CR>', {silent = true})
+map('n', '<leader>h', ':lua vim.lsp.buf.hover()<CR>', {silent = true})
+map('n', '<leader>m', ':lua vim.lsp.buf.rename()<CR>',{silent = true})
+map('n', '<leader>rf', ':lua vim.lsp.buf.references()<CR>', {silent = true})
+map('n', '<leader>s', ':lua vim.lsp.buf.document_symbol()<CR>', {silent = true})
+-- Telescope
+-- TODO: Learn these too
+-- From https://github.com/crivotz/nv-ide/blob/master/lua/settings/keymap.lua#L41
+map('n', '<leader>r', ":lua require('telescope.builtin').live_grep()<CR>", {silent = true})
+map('n', '<leader>bb', ":lua require('telescope.builtin').buffers()<CR>", {silent = true})
+map('n', '<leader>m', ":lua require('telescope.builtin').marks()<CR>", {silent = true})
+map('n', '<leader>e', ":lua require('telescope.builtin').treesitter()<CR>", {silent = true})
+map('n', '<leader>l', ":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>", {silent = true})
+map('n', '<leader>p', ":lua require('telescope').extensions.project.project{}<CR>", {silent = true})
+map('n', '<leader>c', ":lua require('plugins.telescope').my_git_commits()<CR>", {silent = true})
+map('n', '<leader>gg', ":lua require('plugins.telescope').my_git_status()<CR>", {silent = true})
+map('n', '<leader>b', ":lua require('plugins.telescope').my_git_bcommits()<CR>", {silent = true})
+map('n', '<leader>n', ":lua require('plugins.telescope').my_note()<CR>", {silent = true})
+map('n', '<leader>nn', ":e ~/Note/", {silent = false})
+map('n', '<leader>gc', ':Octo issue create<CR>', {silent = false})
+map('n', '<leader>i', ':Octo issue list<CR>', {silent = false})
+
