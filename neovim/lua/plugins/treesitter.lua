95ebc8e8e93927920230eed544b6a533766d6a59
Brendan Mulholland <github@bmulholland.ca>
2021-05-28 14:04:05 +0200

    Enable treesitter integration for matchup

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,9 +1,12 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-  },
-  context_commentstring = {
-    enable = true
-  }
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	highlight = {
+		enable = true,              -- false will disable the whole extension
+	},
+	context_commentstring = {
+		enable = true,
+	},
+	matchup = {
+		enable = true,
+	},
 }
