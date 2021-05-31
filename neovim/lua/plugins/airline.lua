e3d03cbe5e1614f882d05dcd738b0cf3daabd28c
Brendan Mulholland <github@bmulholland.ca>
2021-05-31 18:44:33 +0200

    Didn't need to enable default plugin

--- neovim/lua/plugins/airline.lua
+++ neovim/lua/plugins/airline.lua
@@ -2,7 +2,7 @@
 vim.g.airline_powerline_fonts = 1
 -- Only show git symbols when they're non-zero
 vim.cmd('let airline#extensions#hunks#non_zero_only = 1')
-vim.cmd("let g:airline_extensions = ['branch', 'default', 'fugitiveline', 'gutentags', 'nvimlsp']")
+vim.cmd("let g:airline_extensions = ['branch', 'fugitiveline', 'gutentags', 'nvimlsp']")
 -- Try some caching for performance
 vim.g.airline_highlighting_cache = 1
 if vim.o.background == 'dark' then
