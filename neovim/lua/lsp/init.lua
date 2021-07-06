83dd96e3a39f12e4ca06fd7bb6a5765dc4bcd226
Brendan Mulholland <github@bmulholland.ca>
2021-07-07 00:13:19 +0200

    Try a longer autoformat timeout

--- neovim/lua/lsp/init.lua
+++ neovim/lua/lsp/init.lua
@@ -17,4 +17,4 @@ require'lspconfig'.solargraph.setup{
 require'lspconfig'.sorbet.setup{}
 
 -- Auto format on save
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 500)]]
