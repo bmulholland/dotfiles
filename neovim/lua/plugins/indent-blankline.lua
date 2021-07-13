37ba7e58ec65d7cb6110b815097d40bbeeed6537
Brendan Mulholland <github@bmulholland.ca>
2021-07-13 16:16:03 +0200

    Indentation colours for light colour scheme

--- neovim/lua/plugins/indent-blankline.lua
+++ neovim/lua/plugins/indent-blankline.lua
@@ -1,8 +1,8 @@
 -- From https://github.com/lukas-reineke/indent-blankline.nvim/issues/94#issuecomment-839359011
 
 -- define the highlight groups with only background colors (or leave odd empty to just show the normal background)
-vim.cmd [[highlight IndentOdd guifg=NONE guibg=NONE gui=nocombine]]
-vim.cmd [[highlight IndentEven guifg=NONE guibg=#333333 gui=nocombine]]
+-- vim.cmd [[highlight IndentOdd guifg=NONE guibg=NONE gui=nocombine]]
+-- vim.cmd [[highlight IndentEven guifg=NONE guibg=#333333 gui=nocombine]]
 -- and then use the highlight groups
 vim.g.indent_blankline_char_highlight_list = {"IndentOdd", "IndentEven"}
 vim.g.indent_blankline_space_char_highlight_list = {"IndentOdd", "IndentEven"}
@@ -17,5 +17,5 @@ vim.g.indent_blankline_show_trailing_blankline_indent = false
 -- From https://github.com/lukas-reineke/indent-blankline.nvim/issues/94#issuecomment-858201458
 vim.cmd [[
 autocmd ColorScheme * highlight IndentOdd guifg=NONE guibg=NONE gui=nocombine
-autocmd ColorScheme * highlight IndentEven guifg=NONE guibg=#333333 gui=nocombine
+autocmd ColorScheme * highlight IndentEven guifg=NONE guibg=#EFEFEF gui=nocombine
 ]]
