9833c3e67a1b3954daef6eec637576782170e531
Brendan Mulholland <github@bmulholland.ca>
2021-08-14 15:18:42 +0200

    Ignore images in telescope

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -2,64 +2,64 @@
 local actions = require('telescope.actions')
 require('telescope').setup{
 	defaults = {
-		file_ignore_patterns = {"sorbet/*", "spec/vcr_cassettes/*"}
-		}
+		file_ignore_patterns = {"sorbet/*", "spec/vcr_cassettes/*", "%.svg", "%.png", "%.woff2"}
 	}
+}
 
-		-- Extensions
+-- Extensions
 
-		require('telescope').load_extension('ultisnips')
-		require('telescope').load_extension('projects')
+require('telescope').load_extension('ultisnips')
+require('telescope').load_extension('projects')
 
-		-- Implement delta as previewer for diffs
+-- Implement delta as previewer for diffs
 
-		local previewers = require('telescope.previewers')
-		local builtin = require('telescope.builtin')
-		local conf = require('telescope.config')
-		local M = {}
+local previewers = require('telescope.previewers')
+local builtin = require('telescope.builtin')
+local conf = require('telescope.config')
+local M = {}
 
-		local delta = previewers.new_termopen_previewer {
-			get_command = function(entry)
-				-- this is for status
-				-- You can get the AM things in entry.status. So we are displaying file if entry.status == '??' or 'A '
-				-- just do an if and return a different command
-				if entry.status == '??' or 'A ' then
-					return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value }
-				end
-
-				-- note we can't use pipes
-				-- this command is for git_commits and git_bcommits
-				return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value .. '^!' }
-
-			end
-		}
-
-		M.my_git_commits = function(opts)
-			opts = opts or {}
-			opts.previewer = delta
-
-			builtin.git_commits(opts)
+local delta = previewers.new_termopen_previewer {
+	get_command = function(entry)
+		-- this is for status
+		-- You can get the AM things in entry.status. So we are displaying file if entry.status == '??' or 'A '
+		-- just do an if and return a different command
+		if entry.status == '??' or 'A ' then
+			return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value }
 		end
 
-		M.my_git_bcommits = function(opts)
-			opts = opts or {}
-			opts.previewer = delta
+		-- note we can't use pipes
+		-- this command is for git_commits and git_bcommits
+		return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value .. '^!' }
 
-			builtin.git_bcommits(opts)
-		end
+	end
+}
 
-		M.my_git_status = function(opts)
-			opts = opts or {}
-			opts.previewer = delta
+M.my_git_commits = function(opts)
+	opts = opts or {}
+	opts.previewer = delta
 
-			builtin.git_status(opts)
-		end
+	builtin.git_commits(opts)
+end
 
-		M.project_files = function()
-			local opts = {} -- define here if you want to define something
-			local ok = pcall(require'telescope.builtin'.git_files, opts)
-			if not ok then require'telescope.builtin'.find_files(opts) end
-		end
+M.my_git_bcommits = function(opts)
+	opts = opts or {}
+	opts.previewer = delta
 
-		return M
+	builtin.git_bcommits(opts)
+end
+
+M.my_git_status = function(opts)
+	opts = opts or {}
+	opts.previewer = delta
+
+	builtin.git_status(opts)
+end
+
+M.project_files = function()
+	local opts = {} -- define here if you want to define something
+	local ok = pcall(require'telescope.builtin'.git_files, opts)
+	if not ok then require'telescope.builtin'.find_files(opts) end
+end
+
+return M
 
