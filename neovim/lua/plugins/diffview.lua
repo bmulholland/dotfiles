995eaf5c525e5e1521a122f2929604a9823338f9
Brendan Mulholland <brendan@recital.software>
2021-09-16 22:05:49 +0200

    Update diffview config

--- neovim/lua/plugins/diffview.lua
+++ neovim/lua/plugins/diffview.lua
@@ -1,6 +1,4 @@
 require'diffview'.setup {
   diff_binaries = false,    -- Show diffs for binaries
-  file_panel = {
-    use_icons = true        -- Requires nvim-web-devicons
-  },
+	use_icons = true        -- Requires nvim-web-devicons
 }
