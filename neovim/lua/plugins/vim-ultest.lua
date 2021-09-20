ea1a9220d59c5da52715e9de3fb9e22dafd1dfb1
Brendan Mulholland <brendan@recital.software>
2021-09-20 14:02:58 +0200

    Disable auto-test running for now

--- neovim/lua/plugins/vim-ultest.lua
+++ neovim/lua/plugins/vim-ultest.lua
@@ -1,5 +1,6 @@
 -- run the nearest test every time a file is written
-vim.cmd([[augroup UltestRunner
-    au!
-    au BufWritePost * UltestNearest
-augroup END]])
+-- There's too much warning text for this to be useful; need to solve that first
+-- vim.cmd([[augroup UltestRunner
+--     au!
+--     au BufWritePost * UltestNearest
+-- augroup END]])
