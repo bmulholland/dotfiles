d5f1d19b41440f0ccb3dfa41ef504fb54d432cba
Brendan Mulholland <brendan@recital.software>
2021-09-25 11:39:25 -0700

    Enable test running again

--- neovim/lua/plugins/vim-ultest.lua
+++ neovim/lua/plugins/vim-ultest.lua
@@ -1,6 +1,6 @@
 -- run the nearest test every time a file is written
 -- There's too much warning text for this to be useful; need to solve that first
--- vim.cmd([[augroup UltestRunner
---     au!
---     au BufWritePost * UltestNearest
--- augroup END]])
+vim.cmd([[augroup UltestRunner
+    au!
+    au BufWritePost * UltestNearest
+augroup END]])
