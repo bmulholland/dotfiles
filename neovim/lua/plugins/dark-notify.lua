6bb85e09172917c66972e771f8c03f8d3e65d130
Brendan Mulholland <brendan@recital.software>
2021-09-28 12:02:50 -0700

    Fix lualine ayu theme

--- neovim/lua/plugins/dark-notify.lua
+++ neovim/lua/plugins/dark-notify.lua
@@ -3,5 +3,12 @@ require('dark_notify').run({
 		-- you can use a different colorscheme for each
 		dark  = "sonokai",
 		light  = "ayu",
+		onchange = function(mode)
+			if mode == "dark" then
+				require'lualine'.setup { options = { theme = 'auto'}}
+			else
+				require'lualine'.setup { options = { theme = 'ayu'}}
+			end
+		end
 	},
 })
