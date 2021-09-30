85aa1dc996271c0d4788c67864cf019a33964ee5
Brendan Mulholland <brendan@recital.software>
2021-09-30 11:05:40 -0700

    Use sonokai theme for lualine

--- neovim/lua/plugins/dark-notify.lua
+++ neovim/lua/plugins/dark-notify.lua
@@ -1,11 +1,10 @@
 require('dark_notify').run({
 	schemes = {
-		-- you can use a different colorscheme for each
 		dark  = "sonokai",
 		light  = "ayu",
 		onchange = function(mode)
 			if mode == "dark" then
-				require'lualine'.setup { options = { theme = 'auto'}}
+				require'lualine'.setup { options = { theme = 'sonokai'}}
 			else
 				require'lualine'.setup { options = { theme = 'ayu'}}
 			end
