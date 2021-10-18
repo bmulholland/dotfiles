7d7bf0e633d76c5bc84cb92f51dfd97c226a88e0
Brendan Mulholland <brendan@recital.software>
2021-10-18 15:02:05 -0700

    Autoformat lua and add html LSP

--- neovim/lua/lsp/init.lua
+++ neovim/lua/lsp/init.lua
@@ -10,7 +10,7 @@ vim.cmd('sign define LspDiagnosticsSignHint text=')
 vim.cmd('setlocal omnifunc=v:lua.vim.lsp.omnifunc')
 
 -- Auto format on save
-vim.cmd [[autocmd BufWritePre *.rb,*.rake,*.json,*.js,*.ts,*.vue lua vim.lsp.buf.formatting_sync(null, 1000)]]
+vim.cmd [[autocmd BufWritePre *.rb,*.rake,*.json,*.js,*.ts,*.vue,*.lua lua vim.lsp.buf.formatting_sync(null, 1000)]]
 
 local lsp_installer = require("nvim-lsp-installer")
 
@@ -22,6 +22,7 @@ local servers = {
 	"tsserver",
 	"vuels",
 	"tailwindcss",
+	"html",
 	-- nvim config
 	"sumneko_lua",
 }
