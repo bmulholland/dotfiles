76bd42537927f1d655c56703a8e38e4411ae057e
Brendan Mulholland <brendan@recital.software>
2021-11-25 10:39:32 +0100

    More natural treesitter selecton keys

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	-- Courtesy of https://old.reddit.com/r/neovim/comments/r10llx/the_most_amazing_builtin_feature_nobody_ever/
 	highlight = {
 		enable = true,              -- false will disable the whole extension
 	},
@@ -12,8 +13,10 @@ require'nvim-treesitter.configs'.setup {
 	incremental_selection = {
 		enable = true,
 		keymaps = {
-			node_incremental = ".",
-			node_decremental = ";",
+      init_selection = '<CR>',
+      scope_incremental = '<CR>',
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
 		},
 	},
 }
