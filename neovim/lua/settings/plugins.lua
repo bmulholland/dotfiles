3efabe12424499494e42273a2219739257032a8e
Brendan Mulholland <brendan@recital.software>
2022-04-28 10:34:45 +0200

    User a patched renamer

--- neovim/lua/settings/plugins.lua
+++ neovim/lua/settings/plugins.lua
@@ -69,8 +69,8 @@ return require("packer").startup(function(use)
 	use("lukas-reineke/lsp-format.nvim") -- Formatting
 	-- Nice rename UI
 	use({
-		"filipdutescu/renamer.nvim",
-		branch = "master",
+		-- On a fork until neovim 0.7 support is merged https://github.com/filipdutescu/renamer.nvim/pull/118
+		"abzcoding/renamer.nvim",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
