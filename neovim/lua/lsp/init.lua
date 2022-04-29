cc000493b3ff7786874062bf12192ce4c5a9641b
Brendan Mulholland <brendan@recital.software>
2022-04-29 11:16:44 +0200

    Update to new LSP config format

--- neovim/lua/lsp/init.lua
+++ neovim/lua/lsp/init.lua
@@ -46,54 +46,77 @@ vim.cmd("sign define LspDiagnosticsSignInformation text=")
 vim.cmd("sign define LspDiagnosticsSignHint text=")
 vim.cmd("setlocal omnifunc=v:lua.vim.lsp.omnifunc")
 
--- Install servesr I use
-local lsp_installer = require("nvim-lsp-installer")
-local servers = {
-	"graphql",
-	-- Frontend/Vue
-	"tsserver",
-	"vuels",
-	"tailwindcss",
-	"html",
-	-- Infra as code
-	"terraformls",
-	-- nvim config
-	"sumneko_lua",
-}
+-- Auto-install servers I use
+require("nvim-lsp-installer").setup({
+	ensure_installed = {
+		"graphql",
+		"cucumber_language_server",
+		-- Frontend/Vue
+		"tsserver",
+		"vuels",
+		"tailwindcss",
+		"html",
+		-- Infra as code
+		"terraformls",
+		-- nvim config
+		"sumneko_lua",
+	},
+})
 
-for _, name in pairs(servers) do
-	local ok, server = lsp_installer.get_server(name)
-	-- Check that the server is supported in nvim-lsp-installer
-	if ok then
-		if not server:is_installed() then
-			print("Installing " .. name)
-			server:install()
-		end
-	end
-end
+local lspconfig = require("lspconfig")
 
-lsp_installer.on_server_ready(function(server)
-	local opts = {}
+lspconfig.graphql.setup({
+	capabilities = capabilities,
+	on_attach = require("lsp-format").on_attach,
+})
+lspconfig.cucumber_language_server.setup({
+	capabilities = capabilities,
+	on_attach = require("lsp-format").on_attach,
+})
+lspconfig.html.setup({
+	capabilities = capabilities,
+	on_attach = require("lsp-format").on_attach,
+})
+lspconfig.terraformls.setup({
+	capabilities = capabilities,
+	on_attach = require("lsp-format").on_attach,
+})
+lspconfig.sumneko_lua.setup({
+	capabilities = capabilities,
+	on_attach = require("lsp-format").on_attach,
+})
 
-	opts.capabilities = capabilities
+lspconfig.tsserver.setup({
+	capabilities = capabilities,
+	-- Leave the formatting to ESLint_d via null_ls
+	on_attach = function(client)
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+	end,
+})
+-- vue is formatted via eslint_d via null-ls, below; no format attachment
+-- needed
+lspconfig.vuels.setup({ capabilities = capabilities })
+-- tailwind is formatted via esllint plugin; no format attachment needed
+lspconfig.tailwindcss.setup({ capabilities = capabilities })
 
-	-- (optional) Customize the options passed to the server
-	if server.name == "tsserver" then
-		-- Leave the formatting to ESLint_d via null_ls
-		opts.on_attach = function(client)
-			client.resolved_capabilities.document_formatting = false
-			client.resolved_capabilities.document_range_formatting = false
-		end
-	elseif server.name ~= "vuels" and server.name ~= "tailwindcss" then
-		-- vue is formatted via eslint_d via null-ls, below
-		-- tailwind is formatted via esllint plugin
-		opts.on_attach = require("lsp-format").on_attach
-	end
-
-	-- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
-	server:setup(opts)
-	vim.cmd([[ do User LspAttachBuffers ]])
-end)
+-- Ruby
+lspconfig.sorbet.setup({
+	capabilities = capabilities,
+})
+lspconfig.solargraph.setup({
+	capabilities = capabilities,
+	on_attach = require("lsp-format").on_attach,
+	settings = {
+		solargraph = {
+			diagnostics = true,
+			formatting = true,
+			-- autoformat = true,-- WARNING: experimental
+			-- useBundler = true,
+			-- bundlerPath = "/Users/bmulholland/.rvm/gems/ruby-2.7.2@recital-backend/bin/bundle"
+		},
+	},
+})
 
 -- JS formatting with a daemonized eslint
 local null_ls = require("null-ls")
@@ -113,22 +136,4 @@ null_ls.setup({
 		client.resolved_capabilities.document_formatting = true
 		client.resolved_capabilities.document_range_formatting = true
 	end,
-})
-
--- Ruby
-require("lspconfig").sorbet.setup({
-	capabilities = capabilities,
-})
-require("lspconfig").solargraph.setup({
-	capabilities = capabilities,
-	on_attach = require("lsp-format").on_attach,
-	settings = {
-		solargraph = {
-			diagnostics = true,
-			formatting = true,
-			-- autoformat = true,-- WARNING: experimental
-			-- useBundler = true,
-			-- bundlerPath = "/Users/bmulholland/.rvm/gems/ruby-2.7.2@recital-backend/bin/bundle"
-		},
-	},
 })
