be9682385579fa92e33998298261bb61b906b1e2
Brendan Mulholland <brendan@recital.software>
2022-07-28 17:14:07 +0200

    Remove branch from lualine; I never use it

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -8,7 +8,9 @@ require("lualine").setup({
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_diagnostic" } } },
+		-- lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_diagnostic" } } },
+		-- branch is too long and I never look at it
+		lualine_b = { "diff", { "diagnostics", sources = { "nvim_diagnostic" } } },
 		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "filetype" },
 		lualine_z = { "location" },
