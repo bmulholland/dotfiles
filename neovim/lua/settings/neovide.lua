57402fec70ef8248f7d848e1eb1a7f28518cf321
Brendan Mulholland <brendan@recital.software>
2023-08-11 12:04:35 +0200

    Don't apply neovide settings unless in neovide

--- neovim/lua/settings/neovide.lua
+++ neovim/lua/settings/neovide.lua
@@ -1,2 +1,4 @@
-vim.cmd("let g:neovide_input_use_logo=v:true")
-vim.cmd("let g:neovide_cursor_animation_length=0.02")
+if vim.g.neovide then
+  vim.cmd("let g:neovide_input_use_logo=v:true")
+  vim.cmd("let g:neovide_cursor_animation_length=0.02")
+end
