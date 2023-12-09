931ae1da7ceac6a7678fee420dd459bc480aaaad
Brendan Mulholland <brendan@recital.software>
2023-12-09 01:00:46 +0100

    Working volar/prettier/eslint setup

--- neovim/lua/plugins/volar.lua
+++ neovim/lua/plugins/volar.lua
@@ -7,30 +7,18 @@ return {
           -- Takeover mode https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#volar
           filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
         },
-        eslint = {},
       },
       setup = {
-        eslint = function()
+        volar = function()
           require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            elseif client.name == "volar" then
+            -- Don't use volar for formatting
+            if client.name == "volar" then
               client.server_capabilities.documentFormatting = false
               -- client.server_capabilities.documentFeatures.documentFormatting = false
               client.server_capabilities.documentFormattingProvider = false
             end
           end)
         end,
-      },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        ["vue"] = { { "eslint" } },
       },
     },
   },
