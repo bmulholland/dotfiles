324a886d5755e27817f4f5441de17516cb05d48d
Brendan Mulholland <brendan@recital.software>
2024-01-05 11:25:31 -0800

    Fix conflicts between sorbet and solargraph

--- neovim/lua/plugins/sorbet.lua
+++ neovim/lua/plugins/sorbet.lua
@@ -4,6 +4,18 @@ return {
     opts = {
       servers = {
         sorbet = {},
+        solargraph = {},
+      },
+      setup = {
+        -- Disable Solargraph lookup of references and definitions; Sorbet does that better
+        solargraph = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "solargraph" then
+              client.server_capabilities.referencesProvider = false
+              client.server_capabilities.definitionProvider = false
+            end
+          end)
+        end,
       },
     },
   },
