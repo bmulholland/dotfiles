b8a473567b97018e54d28f9d6d3119625a183843
Brendan Mulholland <brendan@recital.software>
2024-01-12 14:01:07 -0800

    Better ruby setup, no solargraph, but DAP + neotest

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -11,13 +11,14 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
 
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.test.core" },
 
     { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.lang.ruby" }, -- want this, but without solargraph...
+    -- { import = "lazyvim.plugins.extras.lang.ruby" }, -- forked this to remove solargraph
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     -- I use volar for TS
--- neovim/lua/plugins/ruby.lua
+++ neovim/lua/plugins/ruby.lua
@@ -1,10 +1,57 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "ruby",
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         sorbet = {},
         rubocop = {},
+      },
+    },
+  },
+  {
+    "suketa/nvim-dap-ruby",
+    config = function()
+      require("dap-ruby").setup()
+    end,
+  },
+  {
+    "olimorris/neotest-rspec",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      "suketa/nvim-dap-ruby",
+      config = function()
+        require("dap-ruby").setup()
+      end,
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "olimorris/neotest-rspec",
+    },
+    opts = {
+      adapters = {
+        ["neotest-rspec"] = {
+          rspec_cmd = function()
+            return vim.tbl_flatten({
+              "bundle",
+              "exec",
+              "rspec",
+            })
+          end,
+        },
       },
     },
   },
