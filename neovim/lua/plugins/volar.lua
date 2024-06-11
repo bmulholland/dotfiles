e2c08109f6e60a317c458192b6fc7a12fa8a0793
Brendan Mulholland <brendan@recitalapp.com>
2024-06-11 09:27:42 +0200

    Upgrade lazyvim and switch to vtsls alongside

--- neovim/lua/plugins/volar.lua
+++ neovim/lua/plugins/volar.lua
@@ -1,29 +1,33 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "vue" } },
+  },
+
+  -- Add LSP servers
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        tsserver = {
-          init_options = {
-            hostInfo = "neovim",
-            plugins = {
-              {
-                name = "@vue/typescript-plugin",
-                location = "anything", -- makes it look in node_modules
-                languages = {
-                  "typescript",
-                  "vue",
-                },
-              },
-            },
-          },
-          filetypes = {
-            "javascript",
-            "typescript",
-            "vue",
-          },
-        },
+        volar = {},
+        vtsls = {},
       },
     },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      table.insert(opts.servers.vtsls.filetypes, "vue")
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@vue/typescript-plugin",
+          location = "anything", -- makes it look in node_modules
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
+    end,
   },
 }
