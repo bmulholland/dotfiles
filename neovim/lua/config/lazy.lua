13edf662f620ec524bb1672e589b52b9f930d83c
Brendan Mulholland <brendan@recitalapp.com>
2024-11-20 10:38:41 +0100

    Install sql lang

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
 
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
