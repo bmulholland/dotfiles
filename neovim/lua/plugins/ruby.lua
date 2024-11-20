de5b45e50e386df18e3ea7b75f37d9e63b20b336
Brendan Mulholland <brendan@recitalapp.com>
2024-11-20 10:38:35 +0100

    Fix rubocop and sorbet setup -- no mason

--- neovim/lua/plugins/ruby.lua
+++ neovim/lua/plugins/ruby.lua
@@ -1,16 +1,23 @@
+local lspconfig = require("lspconfig")
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      format_notify = true,
       servers = {
         ruby_lsp = {
           enabled = false,
         },
         sorbet = {
+          mason = false,
           cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
+          root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
         },
         rubocop = {
+          mason = false,
           cmd = { "bundle", "exec", "rubocop", "--lsp" },
+          root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
         },
       },
     },
